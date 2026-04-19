2f9d22463414c4ff7b457f6efd1dbd7911af9b84
Lucas Kruzolek <lucaskruzolek@gmail.com>
2026-04-19 12:54:05 -0300

    se creo la estructura inicial de archivos y directorios del proyecto

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('principal');
+});
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+Route::get('/comercializacion', function () {
+    return view('comercializacion');
+});
+Route::get('/productos', function () {
+    return view('productos');
+});
+Route::get('/quienes-somos', function () {
+    return view('quienes-somos');
+});
+Route::get('/terminos', function () {
+    return view('terminos-de-uso');
 });
