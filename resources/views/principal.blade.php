f988cb3f789c85054d37834b9520143bfa11fce2
Lucas Kruzolek <lucaskruzolek@gmail.com>
2026-04-23 00:36:18 -0300

    se agregó un carousel a la pagina principal

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -4,7 +4,7 @@
 <body>
 
     <header class="container my-5 text-center">
-        <img src="{{ asset('imagenes/banner-petthreads.jpeg') }}" alt="Banner" class="hero-image mb-4 shadow img-fluid">
+        <img src="{{ asset('imagenes/logo/banner-petthreads.jpeg') }}" alt="Banner" class="hero-image mb-4 shadow img-fluid">
 
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -15,6 +15,26 @@
             </div>
         </div>
     </header>
+
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="imagenes/carousel/imagen2.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="imagenes/carousel/imagen1.jpeg" alt="Second slide">
+            </div>
+          
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 
     <main class="container mb-5">
         <h2 class="mb-4">Nuestros Productos</h2>
